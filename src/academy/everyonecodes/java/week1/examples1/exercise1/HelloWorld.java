5e20867dadacdad69e6836aa217937ede8b5bc77
Atlantisland <55971063+Atlantisland@users.noreply.github.com>
2019-12-11 15:28:27 +0100

    Delete HelloWorld.java

--- src/academy/everyonecodes/java/week1/examples1/exercise1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package academy.everyonecodes.java.week1.examples1.exercise1;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
